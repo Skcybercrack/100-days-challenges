7c9b6f4c62decdc3094bff90104d954e295c1c50
Skcybercrack <114681371+Skcybercrack@users.noreply.github.com>
2022-11-24 00:09:13 +0530

    Create Abstactclass.java

---
+++ Day40/Abstactclass.java
@@ -0,0 +1,18 @@
+use of abstact class in java
+ abstract class Bike{  
+   Bike(){System.out.println("bike is created");}  
+   abstract void run();  
+   void changeGear(){System.out.println("gear changed");}  
+ }  
+//Creating a Child class which inherits Abstract class  
+ class Honda extends Bike{  
+ void run(){System.out.println("running safely..");}  
+ }  
+//Creating a Test class which calls abstract and non-abstract methods  
+ public class TestAbstraction2{  
+ public static void main(String args[]){  
+  Bike obj = new Honda();  
+  obj.run();  
+  obj.changeGear();  
+ }  
+} 
